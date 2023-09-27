e4b8b83f931f989de48ff223e4a12842e0ef3eed
zhangjr <syrthanfeb@163.com>
2023-09-27 17:17:25 +0800

    move file

--- src/router/router-demo.ts
+++ src/router/router-demo.ts
@@ -1,13 +1,13 @@
 export default [
     {
         path: '/rd',
-        component: () => import('@views/router-demo/router-link-view.vue'),
+        component: () => import('@views/router-demo/link-and-view/router-link-view.vue'),
         children: [
-            {path: '/rd/home', component: () => import('@views/router-demo/home.vue')},
-            {path: '/rd/about', component: () => import('@views/router-demo/about.vue')},
+            {path: '/rd/home', component: () => import('@views/router-demo/link-and-view/home.vue')},
+            {path: '/rd/about', component: () => import('@views/router-demo/link-and-view/about.vue')},
         ]
     },
     {
-        path: '/dynamic/:userId', component: () => import('@views/router-demo/dynamic-route.vue')
+        path: '/dynamic/:userId', component: () => import('@views/router-demo/dynamic-router/dynamic-route.vue')
     }
 ]
